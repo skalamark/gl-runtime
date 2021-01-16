a5e40df8c6ea8486558131a25acb510454c7f614
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-16 15:19:11 -0300

    create struct and impl: Runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use gl_core::ast::{AbstractSyntaxTree, Expression, Literal, Statement};
+use gl_core::error::{AnyError, Error, Exception};
+use gl_core::object::Object;
+use gl_core::position::Position;
+use gl_core::state::ProgramState;
+
+pub struct Runtime {}
+
+impl Runtime {
+	pub fn new() -> Self {
+		Self {}
+	}
+}
