680a86775d04a15865be914c30b3a660d22207f1
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-04-01 23:36:12 -0300

    reformat and update expression infix

--- src/rinfix.rs
+++ src/rinfix.rs
@@ -1,74 +1,68 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
-use crate::preludes::*;
-use gl_core::preludes::*;
 use std::ops::Div;
 
+use gl_core::preludes::*;
+
+use crate::preludes::*;
+
 impl Runtime {
+	fn unsupported(&self, infix: Infix, left: Object, right: Object) -> ResultRuntime {
+		let mut exception: Exception = Exception::in_runtime(Except::type_(format!(
+			"unsupported operand type(s) for {}: '{}' and '{}'",
+			infix,
+			left.typer(),
+			right.typer()
+		)));
+		exception.push(ExceptionPoint::new(self.module_context.clone(), Position::default()));
+		Err(exception)
+	}
+
 	pub fn infix(&self, infix: Infix, left: Object, right: Object) -> ResultRuntime {
-		match left {
-			Object::Integer(left_integer) => {
+		match left.clone() {
+			Object::Integer(left_integer) =>
 				if let Object::Integer(right_integer) = right {
 					self.infix_integer(infix, left_integer, right_integer)
 				} else if let Object::Float(right_float) = right {
 					self.infix_integer_float(infix, left_integer, right_float)
+				} else if let Object::Boolean(right_boolean) = right {
+					self.infix_integer_boolean(infix, left_integer, right_boolean)
 				} else {
-					let mut exception: Exception = Exception::new(
-						Except::type_(format!("unsupported operand type(s) for {}", infix)),
-						true,
-					);
-					exception.push(ExceptionPoint::new(
-						self.module_context.clone(),
-						Position::default(),
-					));
-					return Err(exception);
-				}
-			}
-			Object::Float(left_float) => {
-				if let Object::Float(right_float) = right {
+					return self.unsupported(infix, left, right);
+				},
+			Object::Float(left_float) =>
+				if let Object::Integer(right_integer) = right {
+					self.infix_integer_float(infix, right_integer, left_float)
+				} else if let Object::Float(right_float) = right {
 					self.infix_float(infix, left_float, right_float)
-				} else if let Object::Integer(right_integer) = right {
-					self.infix_float_integer(infix, left_float, right_integer)
+				} else if let Object::Boolean(right_boolean) = right {
+					self.infix_float_boolean(infix, left_float, right_boolean)
 				} else {
-					let mut exception: Exception = Exception::new(
-						Except::type_(format!("unsupported operand type(s) for {}", infix)),
-						true,
-					);
-					exception.push(ExceptionPoint::new(
-						self.module_context.clone(),
-						Position::default(),
-					));
-					return Err(exception);
-				}
-			}
-			Object::String(left_string) => {
-				if let Object::String(right_string) = right {
-					self.infix_string(infix, left_string, right_string)
-				} else if let Object::Integer(right_integer) = right {
+					return self.unsupported(infix, left, right);
+				},
+			Object::Boolean(left_boolean) =>
+				if let Object::Integer(right_integer) = right {
+					self.infix_integer_boolean(infix, right_integer, left_boolean)
+				} else if let Object::Float(right_float) = right {
+					self.infix_float_boolean(infix, right_float, left_boolean)
+				} else if let Object::Boolean(right_boolean) = right {
+					self.infix_boolean(infix, left_boolean, right_boolean)
+				} else {
+					return self.unsupported(infix, left, right);
+				},
+			Object::String(left_string) =>
+				if let Object::Integer(right_integer) = right {
 					self.infix_string_integer(infix, left_string, right_integer)
+				} else if let Object::Boolean(right_boolean) = right {
+					self.infix_string_boolean(infix, left_string, right_boolean)
+				} else if let Object::String(right_string) = right {
+					self.infix_string(infix, left_string, right_string)
 				} else {
-					let mut exception = Exception::new(
-						Except::type_(format!("unsupported operand type(s) for {}", infix)),
-						true,
-					);
-					exception.push(ExceptionPoint::new(
-						self.module_context.clone(),
-						Position::default(),
-					));
-					return Err(exception);
-				}
-			}
+					return self.unsupported(infix, left, right);
+				},
 			_ => {
-				let mut exception = Exception::new(
-					Except::type_(format!("unsupported operand type(s) for +")),
-					true,
-				);
-				exception.push(ExceptionPoint::new(
-					self.module_context.clone(),
-					Position::default(),
-				));
-				return Err(exception);
-			}
+				return self.unsupported(infix, left, right);
+			},
 		}
 	}
 
@@ -84,24 +78,6 @@ impl Runtime {
 			Infix::GreaterThanEqual => Object::Boolean(left >= right),
 			Infix::Equal => Object::Boolean(left == right),
 			Infix::NotEqual => Object::Boolean(left != right),
-		})
-	}
-
-	pub fn infix_integer_float(
-		&self, infix: Infix, left: BigInt, right: BigRational,
-	) -> ResultRuntime {
-		let integer2float: BigRational = str_to_big_rational(left.to_string().as_str()).unwrap();
-		Ok(match infix {
-			Infix::Plus => Object::Float(integer2float + right),
-			Infix::Minus => Object::Float(integer2float - right),
-			Infix::Multiply => Object::Float(integer2float * right),
-			Infix::Divide => Object::Float(integer2float / right),
-			Infix::LessThan => Object::Boolean(integer2float < right),
-			Infix::LessThanEqual => Object::Boolean(integer2float <= right),
-			Infix::GreaterThan => Object::Boolean(integer2float > right),
-			Infix::GreaterThanEqual => Object::Boolean(integer2float >= right),
-			Infix::Equal => Object::Boolean(integer2float == right),
-			Infix::NotEqual => Object::Boolean(integer2float != right),
 		})
 	}
 
@@ -122,39 +98,41 @@ impl Runtime {
 		})
 	}
 
-	pub fn infix_float_integer(
-		&self, infix: Infix, left: BigRational, right: BigInt,
-	) -> ResultRuntime {
-		let integer2float: BigRational = str_to_big_rational(right.to_string().as_str()).unwrap();
-		Ok(match infix {
-			Infix::Plus => Object::Float(left + integer2float),
-			Infix::Minus => Object::Float(left - integer2float),
-			Infix::Multiply => Object::Float(left * integer2float),
-			Infix::Divide => Object::Float(left / integer2float),
-			Infix::LessThan => Object::Boolean(left < integer2float),
-			Infix::LessThanEqual => Object::Boolean(left <= integer2float),
-			Infix::GreaterThan => Object::Boolean(left > integer2float),
-			Infix::GreaterThanEqual => Object::Boolean(left >= integer2float),
-			Infix::Equal => Object::Boolean(left == integer2float),
-			Infix::NotEqual => Object::Boolean(left != integer2float),
-		})
-	}
-
 	pub fn infix_string(&self, infix: Infix, left: String, right: String) -> ResultRuntime {
 		match infix {
 			Infix::Plus => Ok(Object::String(format!("{}{}", left, right))),
 			_ => {
-				let mut exception: Exception = Exception::new(
-					Except::type_(format!("unsupported operand type(s) for {}", infix)),
-					true,
-				);
-				exception.push(ExceptionPoint::new(
-					self.module_context.clone(),
-					Position::default(),
-				));
-				return Err(exception);
-			}
+				return self.unsupported(infix, Object::String(left), Object::String(right));
+			},
 		}
+	}
+
+	pub fn infix_boolean(&self, infix: Infix, left: bool, right: bool) -> ResultRuntime {
+		let left2bigint: BigInt = match left {
+			true => 1.to_bigint().unwrap(),
+			false => 0.to_bigint().unwrap(),
+		};
+		let right2bigint: BigInt = match right {
+			true => 1.to_bigint().unwrap(),
+			false => 0.to_bigint().unwrap(),
+		};
+
+		self.infix_integer(infix, left2bigint, right2bigint)
+	}
+
+	pub fn infix_integer_float(
+		&self, infix: Infix, left: BigInt, right: BigRational,
+	) -> ResultRuntime {
+		let left2float: BigRational = str_to_big_rational(&left.to_string()).unwrap();
+		self.infix_float(infix, left2float, right)
+	}
+
+	pub fn infix_integer_boolean(&self, infix: Infix, left: BigInt, right: bool) -> ResultRuntime {
+		let right2bigint: BigInt = match right {
+			true => 1.to_bigint().unwrap(),
+			false => 0.to_bigint().unwrap(),
+		};
+		self.infix_integer(infix, left, right2bigint)
 	}
 
 	pub fn infix_string_integer(&self, infix: Infix, left: String, right: BigInt) -> ResultRuntime {
@@ -165,21 +143,31 @@ impl Runtime {
 				let zero: BigInt = 0.to_bigint().unwrap();
 				while i > zero {
 					i = i - 1;
-					result_string = format!("{}{}", result_string, left.clone());
+					result_string = format!("{}{}", result_string, &left);
 				}
 				Ok(Object::String(result_string))
-			}
+			},
 			_ => {
-				let mut exception: Exception = Exception::new(
-					Except::type_(format!("unsupported operand type(s) for {}", infix)),
-					true,
-				);
-				exception.push(ExceptionPoint::new(
-					self.module_context.clone(),
-					Position::default(),
-				));
-				return Err(exception);
-			}
+				return self.unsupported(infix, Object::String(left), Object::Integer(right));
+			},
 		}
+	}
+
+	pub fn infix_string_boolean(&self, infix: Infix, left: String, right: bool) -> ResultRuntime {
+		let right2bigint: BigInt = match right {
+			true => 1.to_bigint().unwrap(),
+			false => 0.to_bigint().unwrap(),
+		};
+		self.infix_string_integer(infix, left, right2bigint)
+	}
+
+	pub fn infix_float_boolean(
+		&self, infix: Infix, left: BigRational, right: bool,
+	) -> ResultRuntime {
+		let right2bigrational: BigRational = match right {
+			true => str_to_big_rational("1").unwrap(),
+			false => str_to_big_rational("0").unwrap(),
+		};
+		self.infix_float(infix, left, right2bigrational)
 	}
 }
